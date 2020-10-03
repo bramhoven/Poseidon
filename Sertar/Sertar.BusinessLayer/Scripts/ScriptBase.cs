394b061b61f1aac371b5342c913f72800990bc62
Bram Hoven <bram.hoven@hotmail.com>
2020-10-03 23:27:45 +0200

    Add server scripts

---
+++ Sertar/Sertar.BusinessLayer/Scripts/ScriptBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Sertar.BusinessLayer.Scripts
+{
+    public class ScriptBase
+    {
+        #region Fields
+
+        /// <summary>
+        ///     The name of this script.
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        ///     The steps this script has.
+        /// </summary>
+        public List<ScriptStepBase> Steps;
+
+        #endregion
+
+        #region Methods
+
+        #region Static Methods
+
+        /// <summary>
+        ///     Load ScriptBase class from YAML configuration.
+        /// </summary>
+        /// <param name="yaml">The yaml configuration as a string</param>
+        public static ScriptBase LoadFromYaml(string yaml)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            var scriptBase = deserializer.Deserialize<ScriptBase>(yaml);
+            return scriptBase;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Sertar/Sertar.BusinessLayer/Scripts/ScriptStepBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sertar.BusinessLayer.Scripts
+{
+    public class ScriptStepBase
+    {
+        #region Fields
+
+        public List<string> Commands;
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
