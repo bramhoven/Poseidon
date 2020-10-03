2167b449fbaf2ca60fa12a2d271b27a249a7ea98
Bram Hoven <bram.hoven@hotmail.com>
2020-10-04 00:28:40 +0200

    Add classes for user management

---
+++ Sertar/Sertar.BusinessLayer/Authorization/IAuthorizationManager.cs
@@ -0,0 +1,24 @@
+﻿namespace Sertar.BusinessLayer
+{
+    public interface IAuthorizationManager
+    {
+        #region Methods
+
+        /// <summary>
+        ///     Authorize a key.
+        /// </summary>
+        /// <param name="key">The key to authorize</param>
+        /// <returns></returns>
+        bool Authorize(string key);
+
+        /// <summary>
+        ///     Authorize an identity,
+        /// </summary>
+        /// <param name="identifier">The identity name</param>
+        /// <param name="password">The identities' password</param>
+        /// <returns></returns>
+        bool AuthorizeIdentity(string identifier, string password);
+
+        #endregion  
+    }
+}
---
+++ Sertar/Sertar.BusinessLayer/Users/UserManager.cs
@@ -0,0 +1,45 @@
+﻿using Sertar.DataLayer.Users;
+
+namespace Sertar.BusinessLayer.Users
+{
+    public class UserManager
+    {
+        #region Fields
+
+        /// <summary>
+        /// The authorization manager to user to authenticate the user.
+        /// </summary>
+        private readonly IAuthorizationManager _authorizationManager;
+
+        /// <summary>
+        /// The user data access layer object.
+        /// </summary>
+        private readonly IUserDal _userDal;
+
+        #endregion
+
+        #region Constructors
+
+        public UserManager(IUserDal userDal, IAuthorizationManager authorizationManager)
+        {
+            _userDal = userDal;
+            _authorizationManager = authorizationManager;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Login a user.
+        /// </summary>
+        /// <param name="username">The username</param>
+        /// <param name="password">The password</param>
+        public bool Login(string username, string password)
+        {
+            return _authorizationManager.AuthorizeIdentity(username, password);
+        }
+
+        #endregion
+    }
+}
